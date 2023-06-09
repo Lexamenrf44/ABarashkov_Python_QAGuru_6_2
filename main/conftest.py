4340ae6ea368932832050dcba6482ac64e54da30
ABarashkov <aleksander.barashkov@scompler.com>
2023-06-09 22:09:01 +0300

    added 'setup_teardown' fixture

--- main/conftest.py
+++ main/conftest.py
@@ -1,11 +1,21 @@
 import pytest
+from selene import browser
 from selene.support.shared import config
 
-@pytest.fixture(autouse = True)
+
+@pytest.fixture(autouse=True)
+def setup_teardown():
+    browser.open(config.base_url)
+    yield
+    browser.quit()
+
+
+@pytest.fixture(autouse=True)
 def setup_browser():
     config.timeout = 10
     config.window_width = 1920
     config.window_height = 1080
     config.base_url = 'https://www.google.com/ncr'
 
-    yield
+
+
