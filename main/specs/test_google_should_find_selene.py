1be8d1983db8d4b044c7ce8a2b2f6b464411aed0
ABarashkov <aleksander.barashkov@scompler.com>
2023-06-09 22:09:26 +0300

    fixed test steps using pageObjects created

--- main/specs/test_google_should_find_selene.py
+++ main/specs/test_google_should_find_selene.py
@@ -1,14 +1,15 @@
 from selene import browser
 from selene import be, have
+from main.pages.app.common import App
+
 
 def test_search_with_results():
-    browser.open('')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-    browser.quit()
+    browser.element(App.search_field).should(be.blank)
+    browser.element(App.search_field).type('yashaka/selene').press_enter()
+    browser.element(App.search_list_found).should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_search_without_results():
-    browser.open('')
-    browser.element('[name="q"]').should(be.blank).type('124wfqw r12r asf').press_enter()
-    browser.element('#topstuff').should(have.text('did not match any documents.'))
-    browser.quit()
+    browser.element(App.search_field).should(be.blank)
+    browser.element(App.search_field).type('124wfqw r12r asf').press_enter()
+    browser.element(App.search_list_empty).should(have.text('did not match any documents.'))
