58d88c5d7e30f5cfb13af0e6517c2359cdc231e2
ABarashkov <aleksander.barashkov@scompler.com>
2023-06-09 22:45:18 +0300

    added browser.element(App.search_button).click() step to test_google_should_find_selene.py

--- main/specs/test_google_should_find_selene.py
+++ main/specs/test_google_should_find_selene.py
@@ -5,11 +5,13 @@ from main.pages.app.common import App
 
 def test_search_with_results():
     browser.element(App.search_field).should(be.blank)
-    browser.element(App.search_field).type('yashaka/selene').press_enter()
+    browser.element(App.search_field).type('yashaka/selene')
+    browser.element(App.search_button).click()
     browser.element(App.search_list_found).should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_search_without_results():
     browser.element(App.search_field).should(be.blank)
-    browser.element(App.search_field).type('124wfqw r12r asf').press_enter()
+    browser.element(App.search_field).type('124wfqw r12r asf')
+    browser.element(App.search_button).click()
     browser.element(App.search_list_empty).should(have.text('did not match any documents.'))
